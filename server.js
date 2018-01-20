9c4077f27a1142773688793f98f7771776e34499
nagmani16 <nagmani16@gmail.com>
2018-01-20 15:52:49 -0600

    initial commit

---
+++ server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const hbs = require('hbs');
+
+var app =express();
+
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear',()=>{
+    return new Date().getFullYear();
+})
+
+hbs.registerHelper('screameIt', (text)=> {
+      return text.toUpperCase();
+})
+
+app.get('/', (req,res)=>{
+    res.render('home.hbs', {
+       pageTitle: "Home",
+       welcomeTitle: "Welcome to my website",
+  
+       
+       
+       
+    });
+});
+
+app.get('/about',(req,res)=>{
+    res.render('about.hbs', {
+        pageTitle: "About Page",
+        
+    })
+});
+
+app.get('/bad',(req,res)=>{
+    res.send({
+        errorMessage: 'unable to find page',
+        errorcode:404
+    })
+})
+
+app.listen(3000);
+
